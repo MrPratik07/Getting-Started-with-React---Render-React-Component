34ed1188df8dd8395148d04e7c78da156c82b19d
pratik <pratikgaikwad551@gmail.com>
2021-05-13 12:30:12 +0530

    added component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,9 @@
 import React from "react";
 import '../styles/App.css';
-import Welcome from './Welcome';
 
 const App = () => {
   return (
-    <Welcome name="Pratik" />
+    <p>I am learning React. My life is getting better. </p>
   )
 }
 
